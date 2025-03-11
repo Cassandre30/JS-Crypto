e14e2dcc01df14a14fe78b353e1f82a184dc1d73
Cassandre <cassandrelambertd@gmail.com>
2025-03-11 15:00:02 +0100

    Update crypto_lib_cassandre_lambert.js

--- crypto_lib_cassandre_lambert.js
+++ crypto_lib_cassandre_lambert.js
@@ -3,3 +3,15 @@ function what_are_words_worth () {
     console.log(smtg.length);
     return confirm ("The word "+smtg+" contains "+smtg.length+" letters")
 }
+function count_words(string_of_words) {
+    let words = string_of_words.split(' ');  // Séparer la phrase en mots
+    let count = 0;
+
+    // Boucle pour parcourir les mots
+    for (let i = 0; i < words.length; i++) {
+        if (words[i] !== '') {  // Vérifie si le mot n'est pas une chaîne vide
+            count++;
+        }
+    }
+    return count;
+}
